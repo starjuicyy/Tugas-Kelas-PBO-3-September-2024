9bf09b676ad701c303dd8d9aa69d9b03bd09ccd2
Bintang Asyrof <161684150+starjuicyy@users.noreply.github.com>
2024-09-03 15:17:24 +0700

    Add files via upload

--- Produk.java
+++ Produk.java
@@ -76,7 +76,7 @@ class Tablet extends Produk {
 
     @Override
     public double hitungDiskon() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 }
 
